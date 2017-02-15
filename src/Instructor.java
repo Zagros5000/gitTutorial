5d77eeab6e9b884a7b0c5322a60080e64f7eb63b
Zagros <maroof.zagros@gmail.com>
2017-02-15 13:50:25 -0500

    Added course and made changes to Student and Instructor.

--- src/Instructor.java
+++ src/Instructor.java
@@ -3,9 +3,10 @@
  */
 public class Instructor extends Person {
     String title;
-
-    public Instructor(String name, String emailAddress, String title){
+    Course favCourse;
+    public Instructor(String name, String emailAddress, String title,Course favCourse){
         super(name,emailAddress);
+        this.favCourse=favCourse;
         this.title=title;
     }
 
--- src/Student.java
+++ src/Student.java
@@ -1,14 +1,18 @@
 /**
  * Created by ethan on 2017-02-07.
  */
+import java.util.List;
+
 public class Student extends Person {
     String major;
-
+    List<Course> courseList;
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
     }
-
+    public void addCourse(Course c){
+        this.courseList.add(c);
+    }
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
     }
